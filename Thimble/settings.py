59c7e8c0ed58abcfb3db26dfc9cd08a1eeb9f98a
Jonathan <jonathanlambert247@gmail.com>
2020-06-30 06:04:18 -0700

    created post app and models

--- Thimble/settings.py
+++ Thimble/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'notifications',
     'alerts.apps.AlertsConfig',
     'groups.apps.GroupsConfig',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
---
+++ posts/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Media, PhotoPost
+
+admin.site.register(Media)
+admin.site.register(PhotoPost)
