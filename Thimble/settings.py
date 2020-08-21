1e0c47ccb7b49e26ac07d5f08a9bad7c177b283f
Jonathan <jonathanlambert247@gmail.com>
2020-08-21 15:08:59 -0700

    added cloud url for testing

--- Thimble/settings.py
+++ Thimble/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_SETTING')
 
-ALLOWED_HOSTS = ['192.168.1.11']
+ALLOWED_HOSTS = ['192.168.1.11', 'prod-env.eba-ubkcz2pd.us-west-2.elasticbeanstalk.com']
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = None
 
