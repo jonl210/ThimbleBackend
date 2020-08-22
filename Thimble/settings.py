af3ae85c7b152eb9974ab0a43be7435891d38f4b
Jonathan <jonathanlambert247@gmail.com>
2020-08-22 01:28:16 -0700

    testing

--- Thimble/settings.py
+++ Thimble/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ['DEBUG_SETTING'] == False:
+if 'RDS_HOSTNAME' in os.environ:
     DEBUG = False
 else:
     DEBUG = True
